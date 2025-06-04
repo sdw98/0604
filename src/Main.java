2f4b20f41ac27311926defa71f61fdbb32a9a1db
sdw98 <sdw9851@gmail.com>
2025-06-04 14:37:58 +0900

    while문

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 9; i++){
-            for ( int j = 1; j<=9;j++ ){
-                System.out.printf(j + " x " + i + " = " + (j*i) + "  ");
-            }
-            System.out.println( );
+        int i = 0;
+
+        while (i < 5) {
+            System.out.println(i);
+            i++;
         }
+
     }
 }
