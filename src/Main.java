de04889a6529a3ab6aff9006792bb9419738c634
sdw98 <sdw9851@gmail.com>
2025-06-04 09:47:35 +0900

    논리 연산자

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        boolean hasID = true;
+
+        if(age >= 18 && hasID) {
+            System.out.println("접속가능");
+        }
+
+        boolean isWeekend = false;
+        boolean isHoliday = true;
+
+        if (isWeekend || isHoliday) {
+            System.out.println("쉴 수 있습니다.");
+        }
+
+        boolean isRaining = true;
+        if(!isRaining) {
+            System.out.println("그냥 나가.");
+        } else {
+            System.out.println("우산 챙겨");
+        }
+    }
+}
