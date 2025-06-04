d7fcc3ace8aae792311aa788bea0ff6d4bfff191
sdw98 <sdw9851@gmail.com>
2025-06-04 15:41:30 +0900

    10진수를 16진수로 변경

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 4;
+        int number = 65168918;
 
         String binaryString = Integer.toBinaryString(number);
+        String hexString = Integer.toHexString(number);
 
         System.out.println("Binary : " + binaryString );
+        System.out.println("Hex : " + hexString);
     }
 }
