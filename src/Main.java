5fa4f566a693bedea3412f8ee65c5c15a10e2f9b
sdw98 <sdw9851@gmail.com>
2025-06-04 13:45:01 +0900

    이중 for문

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if(i%3 == 0){
-                System.out.println(i);
+        for (int i = 1; i <= 9; i++){
+            for ( int j = 1; j<=9;j++ ){
+                System.out.printf(j + " x " + i + " = " + (j*i) + "  ");
             }
+            System.out.println( );
         }
     }
 }
