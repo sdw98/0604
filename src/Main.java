8b26400df2b63debcf04d09d97f07d7091f02a54
sdw98 <sdw9851@gmail.com>
2025-06-04 11:49:22 +0900

    비교 연산자

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 7;
-
-        System.out.println("a == b : " + (a == b));
-        System.out.println("a != b : " + (a != b));
-
-        System.out.println("a > b : " + (a > b));
-        System.out.println("a < b : " + (a < b));
-        System.out.println("a >= 10 : " + ( a>= 10));
-        System.out.println(";b <= 5 : " + ( b <= 5));
+        for (int i = 1; i <= 10; i++) {
+            if(i%3 == 0){
+                System.out.println(i);
+            }
+        }
     }
 }
